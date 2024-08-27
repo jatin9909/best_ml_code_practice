30a5006f36a063f83f7c0ece25e95f50d879deb6
Jatin <jatin.dhar27@gmail.com>
2024-08-27 18:22:48 +0530

    updated code structure

--- src/model/pipeline/collection.py
+++ src/model/pipeline/collection.py
@@ -1,16 +1,31 @@
+"""
+This module provides functionalities to load data from a database.
+
+It includes a function to extract data from the RentApartments table
+in the database and load it into a pandas DataFrame. This module is useful
+for scenarios where data needs to be retrieved from a database for further
+analysis or processing. It uses SQLAlchemy for executing database queries
+and pandas for handling the data in a DataFrame format.
+"""
+
 import pandas as pd
 from loguru import logger
+from sqlalchemy import select
 
-from config.config import engine
+from config import engine
 from db.db_model import RentApartments
-from sqlalchemy import select 
 
 
-def load_data(path): #data_file_name
-    logger.info(f"loading csv file at path {path}")
-    return pd.read_csv(path)
+def load_data_from_db() -> pd.DataFrame:
+    """
+    Extract the entire RentApartments table from the database.
 
-def load_data_from_db():
-    logger.info("extracting the table from the database")
+    Returns:
+        pd.DataFrame: DataFrame containing the RentApartments data.
+    """
+    logger.info('extracting the table from the database')
     query = select(RentApartments)
-    return pd.read_sql(query, engine)
+    return pd.read_sql(
+        query,
+        engine,
+    )
--- src/model/pipeline/preparation.py
+++ src/model/pipeline/preparation.py
@@ -1,36 +1,65 @@
-import pandas as pd
+"""
+This module provides functionality for preparing a dataset for ML model.
+
+It consists of functions to load data from a database,
+encode categorical columns, and parse specific columns for further processing.
+"""
+
 import re
+
+import pandas as pd
+from loguru import logger
+
 from model.pipeline.collection import load_data_from_db
 
-from loguru import logger 
 
-def prepare_data():
-    
-    logger.info("starting up preprocessing pipeline")
-    # to prepare dataset we need: 
-    #1. load the dataset
-    data = load_data_from_db()
-    #2. encode columns like balcony, parking etc
-    data_encoded = encode_cat_cols(data)
-    #3. parse the garden column
-    df = parse_garden_col(data_encoded)
+def prepare_data() -> pd.DataFrame:
+    """
+    Prepare the dataset for analysis and modelling.
 
-    return df
+    This involves loading the data, encoding categorical columns,
+    and parsing the 'garden' column.
 
-def encode_cat_cols(data):
-    
-    cols = ['balcony','parking', 'furnished', 'garage', 'storage']
-    logger.info(f"encoding categorical columns: {cols}")
-    return pd.get_dummies(data, 
-                          columns = cols, 
-                          drop_first=True)
+    Returns:
+        pd.DataFrame: The processed dataset.
+    """
+    logger.info('starting up preprocessing pipeline')
+    dataframe = load_data_from_db()
+    data_encoded = _encode_cat_cols(dataframe)
+    return _parse_garden_col(data_encoded)
 
-def parse_garden_col(data):
-    
-    logger.info("pasring garden column")
-    for i in range(len(data)):
-        if data.garden[i]=='Not present':
-            data.garden[i]=0
-        else: 
-            data.garden[i] = int(re.findall(r'\d+', data.garden[i])[0])
-    return data 
+
+def _encode_cat_cols(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Encode categorical columns into dummy variables.
+
+    Arg:
+        dataframe (pd.DataFrame): The original dataset.
+
+    Returns:
+        pd.DataFrame: Dataset with categorical columns encoded.
+    """
+    cols = ['balcony', 'parking', 'furnished', 'garage', 'storage']
+    logger.info(f'encoding categorical columns: {cols}')
+    return pd.get_dummies(
+        dataframe,
+        columns=cols,
+        drop_first=True,
+    )
+
+
+def _parse_garden_col(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Parse the 'garden' column in the dataset.
+
+    Args:
+        dataframe (pd.DataFrame): The dataset with a 'garden' column.
+
+    Returns:
+        pd.DataFrame: The dataset with the 'garden' column parsed.
+    """
+    logger.info('pasring garden column')
+    dataframe['garden'] = dataframe['garden'].apply(
+        lambda x: 0 if x == 'Not present' else int(re.findall(r'\d+', x)[0]),
+    )
+    return dataframe
